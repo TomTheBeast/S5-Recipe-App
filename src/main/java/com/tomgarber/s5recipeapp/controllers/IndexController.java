2497d9fc35a1fd966b51b3f3892bad7888203108
Tom Garber <thogarbe@amazon.com>
2021-09-22 11:51:27 -0500

    Changes

---
+++ src/main/java/com/tomgarber/s5recipeapp/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.tomgarber.s5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+        System.out.println("Index Loaded");
+        return "index";
+    }
+}
