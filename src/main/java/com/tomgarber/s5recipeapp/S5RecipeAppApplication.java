6ca54bd7c04b6da53192aa3e8464a4d418043429
Tom Garber <thogarbe@amazon.com>
2021-09-22 10:14:47 -0500

    Initial Commit

---
+++ src/main/java/com/tomgarber/s5recipeapp/S5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.tomgarber.s5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S5RecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tomgarber/s5recipeapp/S5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomgarber.s5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
